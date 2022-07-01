772facef3885e3583dcaac7d855ca2b2af808547
Michael Chen <michaelexgt@gmail.com>
2022-07-01 16:57:46 +0800

    airPortApi

---
+++ airportAPI/airport.js
@@ -0,0 +1,32 @@
+// const apiTest = document.getElementById("apiTest");
+// // fetch("src/park.json")
+// fetch(
+//   "https://ptx.transportdata.tw/MOTC/v2/Air/FIDS/Airport/Departure/TPE?IsCargo=false&%24top=30&%24format=JSON"
+// )
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (myJson) {
+//     console.log(myJson);
+//     myJson.map((v, i) => {
+//       return (apiTest.innerHTML += `<div class="cards box${i}">
+//       <h1>${v.AirlineID}</h1>
+//     </div>`);
+//     });
+//   });
+
+
+//   ActualDepartureTime: "2022-06-30T00:30"
+// AirlineID: "CI"
+// ArrivalAirportID: "AKL"
+// CheckCounter: ""
+// DepartureAirportID: "TPE"
+// DepartureRemark: "出發DEPARTED"
+// EstimatedDepartureTime: "2022-06-30T00:30"
+// FlightDate: "2022-06-30"
+// FlightNumber: "59"
+// Gate: " D1"
+// IsCargo: false
+// ScheduleDepartureTime: "2022-06-30T00:45"
+// Terminal: "2"
+// UpdateTime: "2022-07-01T15:20:19+08:00"
